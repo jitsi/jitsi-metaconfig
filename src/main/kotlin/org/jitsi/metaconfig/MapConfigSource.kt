d8dd903342048471c7686722dc837f319fdd14ce
Brian Baldino <bbaldino@gmail.com>
2020-07-13 08:50:55 -0700

    add ctor which takes a mapbuilder arg to mapconfigsource

--- src/main/kotlin/org/jitsi/metaconfig/MapConfigSource.kt
+++ src/main/kotlin/org/jitsi/metaconfig/MapConfigSource.kt
@@ -10,6 +10,7 @@ class MapConfigSource(
     private val configValues: Map<String, Any> = mapOf()
 ) : ConfigSource {
     constructor(name: String, vararg props: Pair<String, Any>) : this(name, props.toMap())
+    constructor(name: String, mapBuilder: MutableMap<String, Any>.() -> Unit) : this(name, LinkedHashMap<String, Any>().apply(mapBuilder))
 
     override fun getterFor(type: KType): (String) -> Any {
         return { configKey ->
