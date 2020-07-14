63520b2799620667210de4019cea706757efde88
Brian Baldino <bbaldino@gmail.com>
2020-07-14 15:51:06 -0700

    add optional context to lambda supplier

--- src/main/kotlin/org/jitsi/metaconfig/supplier/LambdaSupplier.kt
+++ src/main/kotlin/org/jitsi/metaconfig/supplier/LambdaSupplier.kt
@@ -1,10 +1,12 @@
 package org.jitsi.metaconfig.supplier
 
 class LambdaSupplier<ValueType : Any>(
+    private val context: String,
     private val supplier: () -> ValueType
 ) : ConfigValueSupplier<ValueType>() {
+    constructor(supplier: () -> ValueType) : this("", supplier)
 
     override fun doGet(): ValueType = supplier()
 
-    override fun toString(): String = "${this::class.simpleName}"
+    override fun toString(): String = "${this::class.simpleName} $context"
 }
--- src/test/kotlin/org/jitsi/metaconfig/supplier/LambdaSupplierTest.kt
+++ src/test/kotlin/org/jitsi/metaconfig/supplier/LambdaSupplierTest.kt
@@ -2,6 +2,7 @@ package org.jitsi.metaconfig.supplier
 
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
 
 class LambdaSupplierTest : ShouldSpec({
     var lambdaCallCount = 0
@@ -15,6 +16,12 @@ class LambdaSupplierTest : ShouldSpec({
         should("get the value from the given lambda") {
             ls.get() shouldBe 42
             lambdaCallCount shouldBe 1
+        }
+        context("with a context hint") {
+            val lsc = LambdaSupplier("hard-coded value of 42", lambda)
+            should("include the context in the toString") {
+                lsc.toString() shouldContain "hard-coded value of 42"
+            }
         }
     }
 })
